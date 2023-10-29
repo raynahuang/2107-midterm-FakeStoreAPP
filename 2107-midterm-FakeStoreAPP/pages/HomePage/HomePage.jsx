c1ace75fdd0b532910be649d1d128dc88403a05f
raynahuang <raynainca190217@gmail.com>
2023-10-28 20:19:30 -0700

    increase 4 images

--- 2107-midterm-FakeStoreAPP/pages/HomePage/HomePage.jsx
+++ 2107-midterm-FakeStoreAPP/pages/HomePage/HomePage.jsx
@@ -1,32 +1,16 @@
-import { useState, useEffect } from 'react'; // Only import useState and useEffect
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-function HomePage() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-      .then((response) => response.json())
-      .then((data) => setProducts(data))
-      .catch((error) => console.error('Error fetching products:', error));
-  }, []);
-
+const HomePage = props => {
   return (
     <div>
-      <h1>Home Page</h1>
-      <h2>Product List</h2>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <h3>{product.title}</h3>
-            <p>{product.description}</p>
-            <p>${product.price}</p>
-            {/* You can add more product information here */}
-          </li>
-        ))}
-      </ul>
+      
     </div>
-  );
+  )
 }
 
-export default HomePage;
+HomePage.propTypes = {
+
+}
+
+export default HomePage
