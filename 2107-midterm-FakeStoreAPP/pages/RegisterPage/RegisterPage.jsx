a09500785efe6c5f2688f61e67077bdaf7912bda
raynahuang <raynainca190217@gmail.com>
2023-10-28 20:05:38 -0700

    create 3 page

---
+++ 2107-midterm-FakeStoreAPP/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const RegisterPage = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+RegisterPage.propTypes = {
+
+}
+
+export default RegisterPage
--- 2107-midterm-FakeStoreAPP/src/App.jsx
+++ 2107-midterm-FakeStoreAPP/src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
-//import LoginPage from './LoginPage';
+import LoginPage from '../pages/LoginPage/LoginPage';
 import ProductDetailPage from '../pages/ProductDetailPage/ProductDetailPage';
-//import RegisterPage from './RegisterPage';
-//import WishListPage from './WishListPage';
+import RegisterPage from '../pages/RegisterPage/RegisterPage';
+
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/product"  element={<ProductDetailPage />} />
-        
+        <Route path="/product" element={<ProductDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
   );
