5e6f55676e9937dcee9acc839777a6cb82dad59d
raynahuang <raynainca190217@gmail.com>
2023-10-29 11:45:59 -0700

    update productpage

--- 2107-midterm-FakeStoreAPP/pages/ProductPage/ProductPage.jsx
+++ 2107-midterm-FakeStoreAPP/pages/ProductPage/ProductPage.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ProductCard from "../../src/components/ProductCard/ProductCard";
-import { Grid, Container } from "@mui/material";
+import { Grid, Container, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 
 const ProductPage = () => {
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate(); // Initialize the navigate function
 
   useEffect(() => {
     // Fetch product data from the API
-    axios.get("https://fakestoreapi.com/products")
+    axios
+      .get("https://fakestoreapi.com/products")
       .then((response) => {
         setProducts(response.data);
       })
@@ -17,8 +20,16 @@ const ProductPage = () => {
       });
   }, []);
 
+  const handleGoBack = () => {
+    // Use the navigate function to go back to the home page
+    navigate("/");
+  };
+
   return (
     <Container>
+      <Button variant="outlined" color="primary" onClick={handleGoBack}>
+        Go Back
+      </Button>
       <Grid container spacing={3}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
